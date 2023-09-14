88facaee48849d84ab5a601fb80c67f283bd532d
olivo4ka <Kostylev.egor.1337@gmail.com>
2023-09-14 17:06:35 +0300

    Implemented Queue structure

---
+++ AlgorithmsAndDataStructure/Queue.go
@@ -0,0 +1,28 @@
+package AlgorithmsAndDataStructure
+
+import "fmt"
+
+func main() {
+	myQueue := Queue{}
+	fmt.Println(myQueue)
+	myQueue.enqueue(111)
+	myQueue.enqueue(222)
+	myQueue.enqueue(333)
+	fmt.Println(myQueue)
+	myQueue.dequeue()
+	fmt.Println(myQueue)
+}
+
+type Queue struct {
+	items []int
+}
+
+func (q *Queue) enqueue(i int) {
+	q.items = append(q.items, i)
+}
+
+func (q *Queue) dequeue() int {
+	firstItem := q.items[0]
+	q.items = q.items[1:]
+	return firstItem
+}
