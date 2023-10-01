d768f73881579040e2fb114fda9a9c3f0db6ff77
Egor Kostylev <46248025+olivo4ka37@users.noreply.github.com>
2023-10-01 05:04:29 +0300

    Time efficiently solution LeetCode problem 3sum, сука я так заебался...

--- LeetCode/3sum.go
+++ LeetCode/3sum.go
@@ -1,57 +1,37 @@
 package LeetCode
 
 import (
-	"fmt"
 	"sort"
 )
 
 func ThreeSum(nums []int) [][]int {
-	result, xAnswers := make([][]int, 0), 0
+	result := make([][]int, 0)
 
 	sort.Slice(nums, func(i, j int) bool {
 		return nums[i] > nums[j]
 	})
 
-	fmt.Println("nums is:", nums)
+	//fmt.Println("nums is:", nums)
 
 	for i := 0; i < len(nums)-2; i++ {
 		j, k := i+1, len(nums)-1
-		fmt.Println("i is:", i, "j is:", j, "k is:", k)
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		//fmt.Println("i is:", i, "j is:", j, "k is:", k)
 		for j < k {
 			if nums[i]+nums[j]+nums[k] == 0 {
-				result = append(result, nil)
-				result[xAnswers] = make([]int, 3)
-				result[xAnswers][0] = nums[i]
-				result[xAnswers][1] = nums[j]
-				result[xAnswers][2] = nums[k]
-				j++
+				result = append(result, []int{nums[i], nums[j], nums[k]})
 				k--
-				xAnswers++
-			} else if nums[j]+nums[k]+nums[i] < 0 {
+				for j < k && nums[k] == nums[k+1] {
+					k--
+				}
+			} else if nums[j]+nums[k]+nums[i] <= 0 {
 				k--
 			} else {
 				j++
 			}
-			fmt.Println("j is:", j, "k is:", k, "RESULT IS:", result)
-		}
-	}
-
-	fmt.Println("RESULT IS111:", result)
-
-	sort.Slice(result, func(i, j int) bool {
-		return result[i][1] > result[j][1]
-	})
-
-	sort.SliceStable(result, func(i, j int) bool {
-		return result[i][0] > result[j][0]
-	})
-
-	fmt.Println("RESULT IS222:", result)
-
-	for i := 0; i < len(result)-1; i++ {
-		if result[i][0] == result[i+1][0] && result[i][1] == result[i+1][1] && result[i][2] == result[i+1][2] {
-			result = append(result[:i], result[i+1:]...)
-			i--
+			//fmt.Println("j is:", j, "k is:", k, "RESULT IS:", result)
 		}
 	}
 
