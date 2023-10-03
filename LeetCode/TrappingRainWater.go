309a1114f09d1e517e42b0273ae8dc13bebbcf56
Egor Kostylev <46248025+olivo4ka37@users.noreply.github.com>
2023-10-03 06:14:37 +0300

    Solved TrappingRainWater LeetCode problem (hard), O(n) time complexity solution

--- LeetCode/TrappingRainWater.go
+++ LeetCode/TrappingRainWater.go
@@ -1,52 +1,26 @@
 package LeetCode
 
 func Trap(height []int) int {
-	result, j, i, currentWater, leftMax := 0, 0, 0, 0, 0
+	result, left, right, leftMax, rightmax := 0, 0, len(height)-1, 0, 0
 
-	for j < len(height) {
-		if leftMax < height[j] {
-			leftMax = height[j]
-		}
-		if i == len(height) {
-			currentWater = 0
-			j++
-			i = j
-		} else if height[j] <= height[i] && height[j] >= leftMax {
-			result += currentWater
-			currentWater = 0
-			j = i
-			i++
+	for left < right {
+		if height[left] < height[right] {
+			if height[left] >= leftMax {
+				leftMax = height[left]
+			} else {
+				result += (leftMax - height[left])
+			}
+			left++
 		} else {
-			currentWater += height[j] - height[i]
-			i++
+			if height[right] >= rightmax {
+				rightmax = height[right]
+			} else {
+				result += (rightmax - height[right])
+			}
+			right--
 		}
-		//fmt.Println("j is:",j,"i is:",i,"result is:", result)
+
 	}
 
-	//fmt.Println("RESULT IS:", result)
-
-	rightMax, secondResult := 0, 0
-	j, i = len(height)-1, len(height)-1
-
-	for j > 0 {
-		if rightMax < height[j] {
-			rightMax = height[j]
-		}
-		if i == -1 {
-			currentWater = 0
-			j--
-			i = j
-		} else if height[j] < height[i] && height[j] >= rightMax {
-			secondResult += currentWater
-			currentWater = 0
-			j = i
-			i--
-		} else {
-			currentWater += height[j] - height[i]
-			i--
-		}
-		//fmt.Println("j is:",j,"i is:",i,"result is:", result)
-	}
-
-	return result + secondResult
+	return result
 }
