97308c890a312637f3945b05f189cd10b8c0abb3
olivo4ka <Kostylev.egor.1337@gmail.com>
2023-11-03 14:20:27 +0300

    Solved 1400 problem on CodeWars problem name (Я ненавижу 1111)

--- CodeForces/Ihate1111/Ihate111.go
+++ CodeForces/Ihate1111/Ihate111.go
@@ -14,23 +14,27 @@ func main() {
 	var n, x int
 	fmt.Fscan(in, &n)
 
+	//Чикне макнагет теорем Любое число больше (m*n - (m+n)) может состоять из некого набора m и n (x*m + y*n) x и y некие значения.
+
 	for i := 0; i < n; i++ {
-		x1 := 111111111
 		fmt.Fscan(in, &x)
-		for x1 > 1 {
-			if x > x1 {
-				x -= x1
-			} else if x == x1 {
-				fmt.Fprintln(out, "YES")
-				x = 0
-				break
-			} else {
-				x1 /= 10
+		if x > 1099 {
+			fmt.Fprintln(out, "YES")
+		} else {
+			for x > 0 {
+				if x%11 == 0 {
+					fmt.Fprintln(out, "YES")
+					x = 0
+				} else {
+					x -= 111
+					if x == 0 {
+						fmt.Fprintln(out, "YES")
+					}
+				}
+			}
+			if x != 0 {
+				fmt.Fprintln(out, "NO")
 			}
 		}
-		if x != 0 {
-			fmt.Fprintln(out, "NO")
-		}
 	}
-
 }
