a78b71b342433aff52b98b0e9627a8e6b2596508
olivo4ka <Kostylev.egor.1337@gmail.com>
2024-05-15 15:44:41 +0300

    Example of work with experimental arena package

---
+++ ExamplesOfWork/ArenaExamples/1/main.go
@@ -0,0 +1,21 @@
+//go:build goexperiment.arenas
+// +build goexperiment.arenas
+
+package main
+
+import "arena"
+
+type Person struct {
+	Lastname  string
+	Firstname string
+}
+
+func main() {
+	mem := arena.NewArena()
+	defer mem.Free()
+
+	for i := 0; i < 10; i++ {
+		obj := arena.New[Person](mem)
+		print(obj)
+	}
+}
