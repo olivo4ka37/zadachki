07f5c7a9483479a299b631c7b346462fb1bbe389
olivo4ka <Kostylev.egor.1337@gmail.com>
2024-05-15 18:28:41 +0300

    Example of work with arena when we increase liveheap size and instead of calling GC our program multiply by 2 our allocated memory.

--- ExamplesOfWork/ArenaExamples/FisrtExample/main.go
+++ ExamplesOfWork/ArenaExamples/FisrtExample/main.go
@@ -3,24 +3,134 @@
 
 package main
 
+// Данная программа помогает наглядно увидеть процесс работы arena через trace.out файл
+// В программе создается 1000 массивов длиной в 2000 элементов, в конце в память арены
+// добавляем инт значение равное 42 (Кузбасский Регион), и выводим это значение.
+// После запуска программы заходим в trace.out файл испольщуя команду go tool trace trace.out
+// Видим что когда мы подошли GC не вызывался, а размер выделенной памяти в какой-то момент удвоился.
+// Для наглядности под этим кодом закоменчем обычная ситуация с выделением памяти в которой
+// будет вызываться GC.
 import (
 	"arena"
 	"fmt"
+	"os"
+	"runtime/trace"
+	"time"
 )
 
-type Person struct {
-	Lastname  string
-	Firstname string
-}
+const (
+	numberOfArrays = 1000
+	arrayLen       = 2000
+)
 
 func main() {
-	mem := arena.NewArena()
-	defer mem.Free()
+	// Запись в trace файл
+	f, _ := os.Create("trace.out")
+	trace.Start(f)
+	defer trace.Stop()
 
-	for i := 0; i < 10; i++ {
-		obj := arena.New[Person](mem)
-		print(obj)
+	mem := arena.NewArena()
+	for j := 0; j < numberOfArrays; j++ {
+		o1 := arena.MakeSlice[float64](mem, arrayLen, arrayLen)
+		for i := 0; i < arrayLen; i++ {
+			o1[i] = float64(i)
+		}
+		time.Sleep(10 * time.Millisecond)
+	}
+	t := arena.New[int](mem)
+	value := 42
+	t = &value
+	fmt.Println(*t)
+	mem.Free()
+	time.Sleep(3000 * time.Millisecond)
+	fmt.Println("KUZBASS")
+}
+
+/*
+package main
+
+
+import (
+	"fmt"
+	"os"
+	"runtime/debug"
+	"runtime/trace"
+	"sync"
+	"time"
+)
+
+const (
+	NumWorkers    = 4     // Количество воркеров
+	NumTasks      = 500   // Количество задач
+	MemoryIntense = 10000 // Размер память затратной задачи (число элементов)
+)
+
+func main() {
+	// Запись в trace файл
+	f, _ := os.Create("trace.out")
+	trace.Start(f)
+	defer trace.Stop()
+
+	// Установка целевого процента сборщика мусора GOGC=1000.
+	debug.SetGCPercent(1000)
+
+	// Очередь задач и очередь результата
+	taskQueue := make(chan int, NumTasks)
+	resultQueue := make(chan int, NumTasks)
+
+	// Запуск воркеров
+	var wg sync.WaitGroup
+	wg.Add(NumWorkers)
+	for i := 0; i < NumWorkers; i++ {
+		go worker(taskQueue, resultQueue, &wg)
 	}
 
-	fmt.Println("123")
+	// Отправка задач в очередь
+	for i := 0; i < NumTasks; i++ {
+		taskQueue <- i
+	}
+	close(taskQueue)
+
+	// Получение результатов из очереди
+	go func() {
+		wg.Wait()
+		close(resultQueue)
+	}()
+
+	// Обработка результатов
+	for result := range resultQueue {
+		fmt.Println("Результат:", result)
+	}
+
+	fmt.Println("Готово!")
 }
+
+// Функция воркера
+func worker(tasks <-chan int, results chan<- int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for task := range tasks {
+		result := performMemoryIntensiveTask(task)
+		results <- result
+	}
+}
+
+// performMemoryIntensiveTask функция требующая много памяти
+func performMemoryIntensiveTask(task int) int {
+	// Создание среза большого размера
+	data := make([]int, MemoryIntense)
+	for i := 0; i < MemoryIntense; i++ {
+		data[i] = i + task
+	}
+
+	// Имитация временной задержки
+	time.Sleep(10 * time.Millisecond)
+
+	// Вычисление результата
+	result := 0
+	for _, value := range data {
+		result += value
+	}
+	return result
+}
+*/
---
+++ ExamplesOfWork/ArenaExamples/go.mod
@@ -0,0 +1,3 @@
+module github.com/olivo4ka37/zadachki/tree/main/ExamplesOfWork/ArenaExamples
+
+go 1.22.0
